ea415a388a602db9f357c80e6b6bb945b3ea0df2
Umidjon Amirqulov <uamirqulov5@gmail.com>
2024-11-09 19:13:12 +0500

    add calendar component

--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -1,5 +1,6 @@
 import EventCalendar from "@/components/EventCalendar"
 import Announcement from "@/components/Announcement"
+import BigCalendar from "@/components/BigCalendar"
 const StudentPage = ()=>{
     return (
         <div className="p-4 flex gap-4 flex-col xl:flex-row ">
@@ -7,6 +8,7 @@ const StudentPage = ()=>{
             <div className="w-full xl:w-2/3">
                 <div className="h-full bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Schedule (9A)</h1>
+                    <BigCalendar />
                 </div>
             </div>
             {/* Right */}
--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,7 +1,25 @@
+'use client'
+import { Calendar,Views,momentLocalizer } from "react-big-calendar"
+import moment from 'moment'
+import { calendarEvents } from "@/lib/data"
+import { useState } from "react"
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+const localizer = momentLocalizer(moment)
 const BigCalendar = () =>{
+    const [view, setView] = useState(Views.WORK_WEEK)
     return (
         <div>
-            BigCalendar
+            <Calendar 
+            localizer={localizer}
+            events={calendarEvents}
+            startAccessor='start'
+            endAccessor='end'
+            views={['work_week','day']}
+            view={view}
+            style={{height:500}}
+            />    
         </div>
     )
 }
+export default BigCalendar
